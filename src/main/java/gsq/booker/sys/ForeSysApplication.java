812ae5c744db79b67e7f82f7dde984f89bc4dbef
gsq1026 <1025565842@qq.com>
2022-10-16 11:24:26 +0800

    hello world

---
+++ src/main/java/gsq/booker/sys/ForeSysApplication.java
@@ -0,0 +1,11 @@
+package gsq.booker.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeSysApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ForeSysApplication.class,args);
+    }
+}
---
+++ src/main/java/gsq/booker/sys/controller/HelloController.java
@@ -0,0 +1,16 @@
+package gsq.booker.sys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello,world";
+    }
+}
